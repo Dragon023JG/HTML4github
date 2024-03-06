87a099c24a51cc38201fd617e64ba3ac0be79437
Dragon <jonathangutierrez848@gmail.com>
2024-03-05 19:41:22 -0500

    initialize app state variables and cached element references

---
+++ trackerApp/script.js
@@ -0,0 +1,22 @@
+// script.js
+// Section 1: TODOs
+// TODO: Register submissions from the user on the form.
+// TODO: Determine the value of the data submitted and add it to a JavaScript array calle
+// TODO: Call the render function to update the table with the new tasks.
+
+
+// Section 2: App State Variables
+let tasks = [];
+// Section 3: Cached Element References
+const taskForm = null;
+const taskTable = null;
+
+
+document.addEventListener("DOMContentloaded", function () {
+    taskForm = document.getElementById("taskForm");
+    // Get the table element by its ID and assign it to taskTable
+    taskTable = document.getElementById("taskTable");
+    // Log the values of taskForm and taskTable to the console
+    console.log("taskForm:", taskForm);
+    console.log("taskTable:", taskTable);
+});
