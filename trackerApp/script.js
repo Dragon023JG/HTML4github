203dde7e32789f925a4770c8cacb8bff9c4efeae
Dragon <jonathangutierrez848@gmail.com>
2024-03-10 16:16:36 -0400

    added the buttons complete and remove

--- trackerApp/script.js
+++ trackerApp/script.js
@@ -40,16 +40,31 @@ function handleSubmission(event) {
 // // Function to render tasks in the table
 function render() {
     // TODO: Use array methods to create a new table row of data for each item in the arr
-    taskTable.innerHTML = tasks.map(task => `
+    taskTable.innerHTML = tasks.map((task, index) => `
 <tr>
 <td>${task.name}</td>
 <td>${task.description}</td>
 <td>${task.deadline}</td>
-<td><button onclick="markTaskComplete(this)">Complete</button></td>
-<td><button onclick="removeTask(this)">Remove</button></td>
+<td><button onclick="markTaskComplete(${index})">Complete</button></td>
+<td><button onclick="removeTask(${index})">Remove</button></td>
 </tr>
 `).join('');
 }
+
+// Function to mark a task as complete
+function markTaskComplete(index) {
+    // just remove it from the tasks array
+    tasks.splice(index, 1);
+    render(); // Update the table
+}
+
+// Function to remove a task
+function removeTask(index) {
+    tasks.splice(index, 1);
+    render(); // Update the table
+}
+
+
 //  Function to initialize the table
 function init() {
 
