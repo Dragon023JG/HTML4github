2539965be7a09f831e1144976a28ce2b66227300
Dragon <jonathangutierrez848@gmail.com>
2024-04-11 18:57:39 -0400

    add global pending ui

--- ReactRouterTutorial/src/routes/root.jsx
+++ ReactRouterTutorial/src/routes/root.jsx
@@ -1,4 +1,4 @@
-import {Outlet, Link,NavLink, useLoaderData, Form, redirect,} from "react-router-dom"
+import {Outlet, Link,NavLink, useLoaderData, Form, redirect,useNavigation,} from "react-router-dom"
 import {getContacts, createContact} from "../contacts"
 
 export async function action(){
@@ -11,6 +11,7 @@ export async function loader(){
 }
 export default function Root() {
   const{contacts}= useLoaderData();
+  const navigation=useNavigation();
     return (
       <>
         <div id="sidebar">
@@ -74,7 +75,10 @@ export default function Root() {
 
 
         </div>
-        <div id="detail">
+        <div id="detail"
+        className={navigation.state=== "loading"? "loading": ""
+        }
+        >
           <Outlet/>
         </div>
       </>
