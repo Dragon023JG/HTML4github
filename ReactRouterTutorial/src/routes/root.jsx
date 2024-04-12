e31b782848f1683bd81425b53cf0265c4115c04a
Dragon <jonathangutierrez848@gmail.com>
2024-04-11 20:07:49 -0400

    adding search spinner

--- ReactRouterTutorial/src/routes/root.jsx
+++ ReactRouterTutorial/src/routes/root.jsx
@@ -16,6 +16,12 @@ export default function Root() {
   const{contacts,q}= useLoaderData();
   const navigation=useNavigation();
   const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
   useEffect(() =>{
     document.getElementById("q").value =q;
   }, [q]);
@@ -28,6 +34,7 @@ export default function Root() {
             <Form id="search-form" role="search">
               <input
                 id="q"
+                className={searching? "loading": ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
@@ -40,7 +47,7 @@ export default function Root() {
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
               />
               <div
                 className="sr-only"
